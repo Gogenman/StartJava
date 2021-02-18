1a718c24a2a47b7cc67d96dc4acdc01a4f6d04a3
George Sazimai <gondza2828@gmail.com>
2021-02-18 21:39:16 +0300

    implement HW1

---
+++ lesson1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte cpuCores = 2;
+        long cpuFreq = 2073600;
+        int screenResol = 2670;
+        short ramMemory = 8;
+        float numberPi = 3.141592f;
+        double numberPiMore = 3.141592653589d;
+        char symbol = 'X';
+        boolean linux = true;
+
+        System.out.println(cpuCores);
+        System.out.println(cpuFreq);
+        System.out.println(screenResol);
+        System.out.println(ramMemory);
+        System.out.println(numberPi);
+        System.out.println(numberPiMore);
+        System.out.println(symbol);
+        System.out.println(linux);
+    }
+}
