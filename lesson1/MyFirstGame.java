fc8c57a51c3f7c52bdf0d6e8fa8ec387357b20bf
George Sazimai <gondza2828@gmail.com>
2021-02-23 01:26:47 +0300

    implement HW5

---
+++ lesson1/MyFirstGame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int UnknownNumber = 0; 
+        
+        int UserNumber = 0;
+
+        UnknownNumber = 56;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Угадай число от 0 до 100.");
+
+        do {
+            System.out.print("Введите ваше число: ");
+
+            UserNumber = input.nextInt();
+                
+            if (UserNumber > UnknownNumber) 
+                System.out.println("Моё число меньше.");
+            
+            else if (UserNumber < UnknownNumber)                
+                System.out.println("Моё число больше.");                  
+
+            else 
+                System.out.println("Вы угадали!");
+
+        } while (UserNumber != UnknownNumber);
+    }
+}
