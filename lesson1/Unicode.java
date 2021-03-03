1d7a614a191418158f7992cfd84b03683da14852
George Sazimai <gondza2828@gmail.com>
2021-03-03 11:04:43 +0300

    implement HW7

---
+++ lesson1/Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char i = 9398 ;
+        while(i < 10178) {
+            System.out.print(i);
+            i++;
+        }
+    }
+}
