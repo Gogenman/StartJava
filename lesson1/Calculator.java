ad234aa4017df821d6e4e5e321ebf9c0618dc408
George Sazimai <gondza2828@gmail.com>
2021-03-03 10:37:26 +0300

    implement HW6

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -9,35 +9,29 @@ public class Calculator {
         System.out.print("Введите второе число: ");
         double num2 = reader.nextDouble();
         
-        
         System.out.print("\nВыберите операцию (+, -, *, /, ^, %): ");
-        char op = reader.next().charAt(0);
+        char sign = reader.next().charAt(0);
         
-        double answer;
-        int result = 1;
+        double result = 0;
         
-        if(op == '+') {
-            answer = num1 + num2;
-            System.out.println("Результат: " + num1 + " " + op + " " + num2 + " = " + answer);
-        } else if(op == '-') {
-            answer = num1 - num2;
-            System.out.println("Результат: " + num1 + " " + op + " " + num2 + " = " + answer);
-        } else if(op == '*') {
-            answer = num1 * num2;
-            System.out.println("Результат: " + num1 + " " + op + " " + num2 + " = " + answer);
-        } else if(op == '/') {
-            answer = num1 / num2;
-            System.out.println("Результат: " + num1 + " " + op + " " + num2 + " = " + answer);
-        } else if(op == '^') {
+        if(sign == '+') {
+            result = num1 + num2;
+        } else if(sign == '-') {
+            result = num1 - num2;
+        } else if(sign == '*') {
+            result = num1 * num2;
+        } else if(sign == '/') {
+            result = num1 / num2;
+        } else if(sign == '^') {
+            int power = 1;
             while (num2 != 0) {
-            result *= num1;
+            power *= num1;
             --num2;
             }
-            answer = result;
-            System.out.println("Результат: " + num1 + " " + op + " " + num2 + " = " + answer);
-        } else if (op == '%') {
-            answer = num1 % num2;
-            System.out.println("Результат: " + num1 + " " + op + " " + num2 + " = " + answer);
+            result = power;
+        } else if (sign == '%') {
+            result = num1 % num2; 
         }
+        System.out.println("Результат: " + result);
    }
 }
