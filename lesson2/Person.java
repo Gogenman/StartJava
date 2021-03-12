5c3e88d5cada65ea3d4ec67b2032a50b1682387b
George Sazimai <gondza2828@gmail.com>
2021-03-12 12:57:00 +0300

    implement HW2.1

---
+++ lesson2/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    String name = "John";
+    String gender = "Man";
+    float weight = 78.7f;
+    float height = 185.5f;
+    int age = 34;
+
+    boolean sit() {
+
+    }
+    void move() {
+
+    }
+    void talk() {
+
+    }
+    void learnJava() {
+
+    }
+}
