0a81ea4b139cef8bd8c2beb67b5a57571cf4816a
George Sazimai <gondza2828@gmail.com>
2021-03-15 17:40:05 +0300

    soon to be developed

--- lesson2/Wolf.java
+++ lesson2/Wolf.java
@@ -1,24 +1,85 @@
 public class Wolf {
-    String name;
-    String color;
-    float weight;
-    float hight;
+    private String name = "Grey Wolf";
+    private String color = "Grey";
+    private int age = 9;
+    private float weight = 67.5f;
+    private float hight = 84.3f;
 
-    boolean sit() {
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        if(name == "") {
+            System.out.println("Пустая строка");
+        } else {
+            this.name = "hip";
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    void setColor(String color) {
+        if(color == "") {
+            System.out.println("Пустая строка");
+        } else {
+            this.color = color;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    void setWeight(float weight) {
+        if(weight == 0) {
+            System.out.println("Пустая строка");
+        } else {
+            this.weight = weight;
+        }
+    }
+
+    public float getHight() {
+        return hight;
+    }
+
+    void setHight(float hight) {
+        if(hight == 0) {
+            System.out.println("Пустая строка");
+        } else {
+            this.hight = hight;
+        }
+    }
+
+    public boolean sit() {
         System.out.println("Волк присел");
         return true;
     }
 
-    boolean howl() {
+    public boolean howl() {
         System.out.println("Волк завыл на луну");
         return true;
     }
 
-    void run() {
+    public void run() {
         System.out.println("Волк пробежал 1 км");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Волк загрыз одного зайца");
     }
 }
--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -2,15 +2,12 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.name = "GreyWolf";
-        wolfOne.color = "Grey";
-        wolfOne.weight = 67.4f;
-        wolfOne.hight = 58.5f;
         
-        System.out.println("wolf name = " + wolfOne.name);
-        System.out.println("Wolf color = " + wolfOne.color);
-        System.out.println("Weight = " + wolfOne.weight);
-        System.out.println("Hight = " + wolfOne.hight);
+        System.out.println("Wolf name = " + wolfOne.getName());
+        System.out.println("Wolf color = " + wolfOne.getColor());
+        System.out.println("Wolf age = " + wolfOne.getAge());
+        System.out.println("Weight = " + wolfOne.getWeight());
+        System.out.println("Hight = " + wolfOne.getHight());
         wolfOne.sit();
         wolfOne.howl();
         wolfOne.run();
